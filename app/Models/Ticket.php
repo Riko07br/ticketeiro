4a4779d74dec57b30dc892778d3eb3c8be09243a
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-06-28 22:54:49 -0300

    ops: models with relationship created

---
+++ app/Models/Ticket.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model {
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'files',
+    ];
+
+    //relationships
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function stat() {
+        return $this->belongsTo(Stat::class, 'stat_id');
+    }
+
+    public function priority() {
+        return $this->belongsTo(Priority::class, 'priority_id');
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
