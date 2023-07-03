603814c634ced85e58f1d40894b17b6962d525ec
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-07-03 15:22:19 -0300

    ops(middleware): admin, agent and user

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -8,15 +8,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class RedirectIfAuthenticated
-{
+class RedirectIfAuthenticated {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string ...$guards): Response
-    {
+    public function handle(Request $request, Closure $next, string ...$guards): Response {
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
@@ -24,6 +22,14 @@ class RedirectIfAuthenticated
                 return redirect(RouteServiceProvider::HOME);
             }
         }
+
+        // if (Auth::guard($guard)->check() && Auth::user()->role_id == 1) {
+        //     return redirect()->route('admin.dashboard');
+        // } elseif(Auth::guard($guard)->check() && Auth::user()->role_id == 2){
+        //     return redirect()->route('user.dashboard');
+        // } else {
+        //     return $next($request);
+        // }
 
         return $next($request);
     }
