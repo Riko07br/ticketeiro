df95a96343b32fb2140c3cc285c0cbc6bd179f03
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-07-04 23:51:40 -0300

    chore: welcome page change based on role

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,22 +15,27 @@
     </a>
     @auth
         Olá, <b>{{ auth()->user()->name }}</b>
-    @endauth
-    <h2>
-        <a href="/users">Usuários</a>
-        <a href="/tickets">Tickets</a>
-        <a href="/categories">Categorias</a>
-        <a href="/labels">Etiquetas</a>
-    </h2>
 
-    @auth
-        <form class="inline" method="POST" action="/auth/logout">
+        <h2>
+            <a href="/{{ auth()->user()->role->title }}/tickets">Tickets</a>
+            @if (auth()->user()->role->id == '1')
+                <a href="/users">Usuários</a>
+                <a href="/categories">Categorias</a>
+                <a href="/labels">Etiquetas</a>
+            @endif
+        </h2>
+
+        <form method="POST" action="{{ route('logout') }}">
             @csrf
             <button type="submit">
                 Sair
             </button>
         </form>
+    @else
+        <h2> <a href="{{ route('login') }}">Login</a></h2>
     @endauth
+
+
 </body>
 
 </html>
