d49d50f01a70f3aee9ad691eb9d90b77ce56a00c
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-07-06 17:51:01 -0300

    style: add css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,36 @@
-<header>
-    <h1>Login</h1>
-</header>
+<x-layout>
+    <x-layout-header>
+        Login
+    </x-layout-header>
 
-<form method="POST" action="{{ route('authenticate') }}">
-    @csrf
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" value="{{ old('email') }}" />
+    <x-layout-content>
+        <form method="POST" action="{{ route('authenticate') }}">
+            @csrf
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" value="{{ old('email') }}" />
 
-        @error('email')
-            <p>{{ $message }}</p>
-        @enderror
-    </div>
+                @error('email')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div>
-        <label for="password">Senha</label>
-        <input type="password" name="password" value="{{ old('password') }}" />
+            <div>
+                <label for="password">Senha</label>
+                <input type="password" name="password" value="{{ old('password') }}" />
 
-        @error('password')
-            <p>{{ $message }}</p>
-        @enderror
-    </div>
+                @error('password')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div>
-        <button>
-            Entrar
-        </button>
-        <a href="{{ route('register') }}"> Não tenho cadastro </a>
-    </div>
-</form>
+            <div>
+                <button>
+                    Entrar
+                </button>
+                <a href="{{ route('register') }}"> Não tenho cadastro </a>
+            </div>
+        </form>
+    </x-layout-content>
+
+</x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,46 +1,50 @@
-<header>
-    <h1>Registrar</h1>
-</header>
+<x-layout>
+    <x-layout-header>
+        Registro
+    </x-layout-header>
 
-<form method="POST" action="{{ route('store') }}" enctype="multipart/form-data">
-    @csrf
-    <div>
-        <label for="name">Nome</label>
-        <input type="text" name="name" placeholder="Nome completo" value="{{ old('name') }}" />
-        @error('name')
-            <p>{{ $message }}</p>
-        @enderror
-    </div>
+    <x-layout-content>
+        <form method="POST" action="{{ route('store') }}" enctype="multipart/form-data">
+            @csrf
+            <div>
+                <label for="name">Nome</label>
+                <input type="text" name="name" placeholder="Nome completo" value="{{ old('name') }}" />
+                @error('name')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" value="{{ old('email') }}" />
-        @error('email')
-            <p>{{ $message }}</p>
-        @enderror
-    </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" value="{{ old('email') }}" />
+                @error('email')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div>
-        <label for="password">Senha</label>
-        <input type="password" name="password" value="{{ old('password') }}" />
-        @error('password')
-            <p>{{ $message }}</p>
-        @enderror
-    </div>
+            <div>
+                <label for="password">Senha</label>
+                <input type="password" name="password" value="{{ old('password') }}" />
+                @error('password')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div>
-        <label for="password_confirmation">Confirme a senha</label>
-        <input type="password" name="password_confirmation" value="{{ old('password_confirmation') }}" />
-        @error('password_confirmation')
-            <p>{{ $message }}</p>
-        @enderror
-    </div>
+            <div>
+                <label for="password_confirmation">Confirme a senha</label>
+                <input type="password" name="password_confirmation" value="{{ old('password_confirmation') }}" />
+                @error('password_confirmation')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div>
-        <button>
-            Cadastrar
-        </button>
+            <div>
+                <button>
+                    Cadastrar
+                </button>
 
-        <a href="{{ route('login') }}"> Já tenho cadastro </a>
-    </div>
-</form>
+                <a href="{{ route('login') }}"> Já tenho cadastro </a>
+            </div>
+        </form>
+    </x-layout-content>
+</x-layout>
