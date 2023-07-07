b3fb0fd2100763547f8c32bb6b0f3d5875b4f590
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-07-06 23:03:11 -0300

    chore: update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,12 +14,17 @@
             </div>
             <div>
                 <p class="text-xl"> Categorias:</p>
-
+                <p>
+                    Possuem o nome e uma descrição, categoriza o ticket com mais precisão e ajuda a direcionálo para o
+                    agente que pode fornecer o melhor suporte.
+                </p>
             </div>
             <div>
                 <p class="text-xl">Etiquetas:</p>
-                Conhecidas como <b>'tags'</b>, possuem apenas o nome e servem para definir as caracteristicas gerais do
-                ticket. Um ticket pode possuir diversas etiquetas.
+                <p>
+                    Conhecidas como <b>'tags'</b>, possuem apenas o nome e servem para definir as caracteristicas gerais
+                    do ticket. Um ticket pode possuir diversas etiquetas.
+                </p>
             </div>
         </div>
     </x-layout-content>
